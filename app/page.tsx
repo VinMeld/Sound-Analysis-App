56615ed3a904631dcb4bbc2866a524434e865404
Vinay M <36042425+VinMeld@users.noreply.github.com>
2023-08-10 16:51:28 +0000

    Got a few spheres and a square with lines to it

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+"use client"
+import React, { useRef, useState, useEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useDrag } from 'react-use-gesture';
+import { useThree } from "@react-three/fiber";
+import * as THREE from 'three';
 
-export default function Home() {
+function Square({ onMove, position }) {
+  const ref = useRef();
+  const [isDragging, setIsDragging] = useState(false);
+  const { gl, camera } = useThree();
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.z += 0.01; // Rotate square around the z-axis
+    }
+  });
+
+
+  const handlePointerDown = (event) => {
+    setIsDragging(true);
+    window.addEventListener('pointermove', handlePointerMove);
+    window.addEventListener('pointerup', stopDragging);
+  };
+
+  const handlePointerMove = (event) => {
+    if (!isDragging) return;
+
+    event.stopPropagation();
+
+    // Translate the event clientX and clientY into normalized device coordinates
+    const x = (event.clientX / gl.domElement.clientWidth) * 2 - 1;
+    const y = -(event.clientY / gl.domElement.clientHeight) * 2 + 1;
+
+    // Convert the normalized device coordinates into a 3D position
+    const vector = new THREE.Vector3(x, y, 0).unproject(camera);
+
+    ref.current.position.set(vector.x, vector.y, 0);
+    onMove({ x: vector.x, y: vector.y, z: 0 });
+
+  };
+  
+  const stopDragging = (event) => {
+    setIsDragging(false);
+    window.removeEventListener('pointermove', handlePointerMove);
+    window.removeEventListener('pointerup', stopDragging);
+  };
+
+  const handlePointerUp = (event) => {
+    setIsDragging(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <mesh
+    ref={ref}
+    onPointerDown={handlePointerDown}
+    position={position}
+  >
+    <planeGeometry args={[1, 1]} />
+    <meshStandardMaterial color="blue" />
+  </mesh>
+  );
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Sphere({ position }) {
+  return (
+    <mesh position={position}>
+      <sphereGeometry args={[0.5, 16, 16]} />
+      <meshStandardMaterial color="green" />
+    </mesh>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function ConnectionLine({ start, end }) {
+  const lineRef = useRef();
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  useEffect(() => {
+    if (lineRef.current) {
+      const startVec = new THREE.Vector3(...start);
+      const endVec = new THREE.Vector3(...end);
+      lineRef.current.geometry.setFromPoints([startVec, endVec]);
+      lineRef.current.geometry.verticesNeedUpdate = true;
+    }
+  }, [start, end]);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  return (
+    <line ref={lineRef}>
+      <bufferGeometry attach="geometry" />
+      <lineBasicMaterial attach="material" color="black" />
+    </line>
+  );
+}
+
+export default function Scene() {
+  const [squarePos, setSquarePos] = useState(new THREE.Vector3(0, 0, 0)); // Use state
+
+  const handleSquareMove = (position) => {
+    setSquarePos(new THREE.Vector3(position.x, position.y, position.z));
+  };
+
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }} className="bg-white w-screen h-screen">
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+
+      <Square position={squarePos} onMove={handleSquareMove} />
+      <Sphere position={[-2, 2, 0]} />
+      <Sphere position={[2, 2, 0]} />
+      <Sphere position={[-2, -2, 0]} />
+      <ConnectionLine start={[-2, 2, 0]} end={squarePos} />
+      <ConnectionLine start={[2, 2, 0]} end={squarePos} />
+      <ConnectionLine start={[-2, -2, 0]} end={squarePos} />
+    </Canvas>
+  );
 }
